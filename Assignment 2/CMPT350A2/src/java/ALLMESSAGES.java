ddc925f71079db1768897de38c36b4696ebc005a
Ken Dong <yid164@mail.usask.ca>
2019-07-30 13:23:16 -0600

    Assignment 2

---
+++ Assignment 2/CMPT350A2/src/java/ALLMESSAGES.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author dongyinsheng
+ */
+@WebServlet(urlPatterns = {"/ALLMESSAGES"})
+public class ALLMESSAGES extends HttpServlet {
+
+    protected static ArrayList<String> messages = new ArrayList<>();
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ALLMESSAGES</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ALLMESSAGES at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1> All Messages </h1>");
+        out.println("<ul>");
+        String message = request.getParameter("new_message");
+        if(message != null)
+        {
+            messages.add(message);
+        }
+        if(messages.isEmpty())
+        {
+            out.println("</ul>");
+            out.println("<form action=NEWMESSAGES method=POST");
+            out.println("<br /><input type=submit value=back");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        
+        else
+        {
+            for(int i = 0; i<messages.size(); i++)
+            {
+                out.println("<li><b>"+messages.get(i)+"</li>");
+            }
+            out.println("</ul>");
+            out.println("<form action=NEWMESSAGES method=POST");
+            out.println("<br /><input type=submit value=back");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
